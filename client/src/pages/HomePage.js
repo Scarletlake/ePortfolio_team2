c9b85b816d0eb441a197d884cda64a848d1add28
Zhuolun Wu <69140533+ZhuolunWu-0906@users.noreply.github.com>
2020-09-08 15:31:07 +1000

    Added home page and home components
    In order to view home page, "PrivateRoute" in App.js should be changed to "Route"

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,45 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react'
+import HomePortfolios from '../components/HomePortfolios'
 
-const NotFound = () => {
-  return (
-    <Fragment>
-      <h1>
-      Home
-      </h1>
-    </Fragment>
-  );
-};
+class HomePage extends Component {
+  state = {
+    portfolios: [
+      {
+        id: 1,
+        name: 'portfolio 1',
+        url: 'https://eportfolio.com/firstnamelastname/portfolio_1'
+      },
+      {
+        id: 2,
+        name: 'portfolio 2',
+        url: 'https://eportfolio.com/firstnamelastname/portfolio_2'
+      },
+      {
+        id: 3,
+        name: 'portfolio 3',
+        url: 'https://eportfolio.com/firstnamelastname/portfolio_3'
+      },
+      {
+        id: 4,
+        name: 'portfolio 4',
+        url: 'https://eportfolio.com/firstnamelastname/portfolio_4'
+      }
+    ]
+  }
 
-export default NotFound;
+  delPortfolio = id => {
+    console.log(this.state)
+    this.setState({ portfolios: [...this.state.portfolios.filter(portfolio => portfolio.id !== id)] })
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <h2>Your Portfolio</h2>
+        <HomePortfolios portfolios={this.state.portfolios} delPortfolio={this.delPortfolio} />
+      </Fragment>
+    )
+  }
+}
+
+export default HomePage;
