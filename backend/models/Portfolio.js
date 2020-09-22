0f78f46493b2795449898958f113c50c0605b355
Hui OUYNAGH <ouyangh1@student.unimelb.edu.au>
2020-09-23 01:04:06 +1000

    Update Portfolio.js

--- backend/models/Portfolio.js
+++ backend/models/Portfolio.js
@@ -2,41 +2,77 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  firstName: {
+
+  portfolioName: {
     type: String,
     default: '',
-    //required: true
-  },
-  lastName: {
-    type: String,
-    default: '',
-    //required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  phone:{
-    type: String,
-    default: '',
-   
-  },
-  gender: {
-    type: String,
-    enum : ['male','female',''],    
-    default: '',    
-  },
-  password: {
-    type: String,
     required: true
   },
-  portfolio: {
-    type: [{
-      portfolioID: mongoose.Schema.Types.ObjectId,
-    }],
-    default: [],    
+  portfolioURL: {
+    type: String,
+    default: '',
+    required: true,
+  },
+  template: {
+    type: String,
+    default: '',
+    required: true,
+  },
+  homePage:{   
+    type: {
+      title:{
+        type: String,
+        default: 'Home',
+        required: true
+      },
+      firstName: {
+        type: String,
+        default: '',
+        required: true
+      },
+      lastName: {
+        type: String,          
+        default: '',
+        required: true
+      },
+      email: {
+        type: String,          
+        required: true,   
+      },
+      phone:{            
+        type: String,
+        default: '',
+        required: true          
+      },
+      profilePhoto:{
+        type: String,
+        default: '',
+      } 
+    },
+        //required: true
+  },
+
+  formalPage: {
+    type: {
+      title:{
+      type: String,
+      default: 'Academic Background',
+      required: true          
+      }, 
+    },    
+        //required: true    
+  },
+
+  leisurePage: {
+    type: {
+      title:{
+        type: String,
+        default: 'About me',
+        required: true
+      },
+    },
+    //required: true    
   }
 });
 
-module.exports = mongoose.model('user', UserSchema);
+module.exports = mongoose.model('portfolios', UserSchema);
