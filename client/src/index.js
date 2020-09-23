7f0e7848a42b04de8d77b40290f838b86086170f
root <root@Surface.localdomain>
2020-09-24 00:41:23 +1000

    minor changes in theme color and hide password in signin page

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#1976d2" // This is an orange looking color
+        },
+        secondary: {
+            main: "#f50057" //Another orange-ish color
+        }
+    },
+});
+ReactDOM.render(<ThemeProvider theme={theme}><App /></ThemeProvider>, document.getElementById("root"));
