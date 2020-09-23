85b8c9f8d339c584d8731f6dbfc2e5d24f10efbc
Zhuolun WU <zhuolunw@student.unimelb.edu.au>
2020-09-23 18:54:20 +1000

    TemplatePage.js v1
    first version

--- client/src/pages/TemplatePage.js
+++ client/src/pages/TemplatePage.js
@@ -1,15 +1,43 @@
-import React from 'react'
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { Grid, Button, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+  grid: {
+    marginTop: "500px",
+    marginBottom: "500px",
+    textAlign: "center",
+  },
+  btn_style: {
+    background: "linear-gradient(45deg, #5a9cb8 30%, #2d49ba 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+  },
+});
 
-export default function TemplatePage (){
-
-    return (
-      <div>
-       <h1>CHOOSE THE TEMPLATE YOU WANT</h1>
-       <Button variant="contained" color="primary" href="/portfolio/template/editor">
-        use this template
-      </Button>
-      </div>
-    )
+export default function TemplatePage() {
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.grid} justifyContent="space-evenly">
+      <Grid item xs={4}>
+        <Button className={classes.btn_style} href="/portfolio/template/editor">
+          use this template
+        </Button>
+      </Grid>
+      <Grid item xs={4}>
+        <Button className={classes.btn_style} href="/portfolio/template/editor">
+          use this template
+        </Button>
+      </Grid>
+      <Grid item xs={4}>
+        <Button className={classes.btn_style} href="/portfolio/template/editor">
+          use this template
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
