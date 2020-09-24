420e8f7df4bf9611b81f92d94f6f27ad667a31fa
Zhuolun WU <zhuolunw@student.unimelb.edu.au>
2020-09-24 12:47:40 +1000

    Distinguish between uploading avatar and img

---
+++ client/src/components/UploadAvatar.js
@@ -0,0 +1,79 @@
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import request from "superagent";
+import { FormControl, Avatar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { deepOrange } from '@material-ui/core/colors';
+
+const CLOUDINARY_UPLOAD_PRESET = "portfolio";
+const CLOUDINARY_UPLOAD_URL =
+  "https://api.cloudinary.com/v1_1/do0ecn2sm/image/upload";
+var pictureUrl = "";
+
+const useStyles = makeStyles((theme) => ({
+  // root: {
+  //   display: 'flex',
+  //   '& > *': {
+  //     margin: theme.spacing(1),
+  //     width: theme.spacing(17),
+  //     height: theme.spacing(17),
+  //   },
+  // },
+  orange: {
+    color: theme.palette.getContrastText(deepOrange[500]),
+    backgroundColor: deepOrange[500],
+  },
+}));
+
+export default function MyDropzone(props) {
+
+  const classes = useStyles();
+
+  function NameInitial() {
+    var initial = '';
+    if (props.first_name) {
+      initial += props.first_name[0];
+    }
+
+    if (props.last_name) {
+      initial += props.last_name[0];
+    }
+    return initial;
+  }
+
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      let upload = request
+        .post(CLOUDINARY_UPLOAD_URL)
+        .field("upload_preset", CLOUDINARY_UPLOAD_PRESET)
+        .field("file", file);
+      upload.end((err, response) => {
+        if (err) {
+          console.error(err);
+        }
+        pictureUrl = response.body.url;
+        // console.log(pictureUrl);
+      });
+    });
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  return (
+    <FormControl>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {pictureUrl === "" ?
+          (<Avatar alt="Remy Sharp" src="/broken-image.jpg" className={classes.orange}>
+            {NameInitial()}
+          </Avatar>) :
+          (
+            <div>
+              <Avatar src={pictureUrl} alt="Show Picture" />
+            </div>
+          )
+        }
+      </div>
+    </FormControl>
+  );
+}
--- client/src/components/UploadPicture.js
+++ client/src/components/UploadPicture.js
@@ -2,44 +2,76 @@ import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import request from "superagent";
 import { FormControl } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { deepOrange } from '@material-ui/core/colors';
 
 const CLOUDINARY_UPLOAD_PRESET = "portfolio";
 const CLOUDINARY_UPLOAD_URL =
-  "https://api.cloudinary.com/v1_1/do0ecn2sm/image/upload";
+    "https://api.cloudinary.com/v1_1/do0ecn2sm/image/upload";
 var pictureUrl = "";
 
-export default function MyDropzone() {
-  const onDrop = useCallback((acceptedFiles) => {
-    acceptedFiles.forEach((file) => {
-      let upload = request
-        .post(CLOUDINARY_UPLOAD_URL)
-        .field("upload_preset", CLOUDINARY_UPLOAD_PRESET)
-        .field("file", file);
-      upload.end((err, response) => {
-        if (err) {
-          console.error(err);
+const useStyles = makeStyles((theme) => ({
+    // root: {
+    //   display: 'flex',
+    //   '& > *': {
+    //     margin: theme.spacing(1),
+    //     width: theme.spacing(17),
+    //     height: theme.spacing(17),
+    //   },
+    // },
+    orange: {
+        color: theme.palette.getContrastText(deepOrange[500]),
+        backgroundColor: deepOrange[500],
+    },
+}));
+
+export default function MyDropzone(props) {
+
+    const classes = useStyles();
+
+    function NameInitial() {
+        var initial = '';
+        if (props.first_name) {
+            initial += props.first_name[0];
         }
-        pictureUrl = response.body.url;
-        // console.log(pictureUrl);
-      });
-    });
-  }, []);
 
-  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+        if (props.last_name) {
+            initial += props.last_name[0];
+        }
+        return initial;
+    }
 
-  return (
-    <FormControl>
-      <div {...getRootProps()}>
-        <input {...getInputProps()} />
-        <p>Drag or add your avatar here</p>
-      </div>
-      <div>
-        {pictureUrl === "" ? null : (
-          <div>
-            <img src={pictureUrl} width="150" height="150" />
-          </div>
-        )}
-      </div>
-    </FormControl>
-  );
+    const onDrop = useCallback((acceptedFiles) => {
+        acceptedFiles.forEach((file) => {
+            let upload = request
+                .post(CLOUDINARY_UPLOAD_URL)
+                .field("upload_preset", CLOUDINARY_UPLOAD_PRESET)
+                .field("file", file);
+            upload.end((err, response) => {
+                if (err) {
+                    console.error(err);
+                }
+                pictureUrl = response.body.url;
+                // console.log(pictureUrl);
+            });
+        });
+    }, []);
+
+    const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+    return (
+        <FormControl>
+            <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                {pictureUrl === "" ?
+                    (<p>Insert pictrue ...</p>) :
+                    (
+                        <div>
+                            <img src={pictureUrl} alt="Show Picture" />
+                        </div>
+                    )
+                }
+            </div>
+        </FormControl>
+    );
 }
