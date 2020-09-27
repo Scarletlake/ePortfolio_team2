6ba661bddd022075a72929ea024ceed1290e034b
Zhuolun WU <zhuolunw@student.unimelb.edu.au>
2020-09-27 11:15:14 +1000

    Show more details of portfolio for testing

--- client/src/pages/PortfolioPage.js
+++ client/src/pages/PortfolioPage.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import { useParams } from "react-router-dom";
-import {usePortfolio} from "../api/portfolioAPI"
+import { usePortfolio } from "../api/portfolioAPI"
 
-export default function PortfolioPage (){
+export default function PortfolioPage() {
 
   const { id } = useParams();
   const { loading, res, error } = usePortfolio(id);
-    
+
   if (loading) {
-      return <p>Loading...</p>;    
+    return <p>Loading...</p>;
   }
-  if (error) {        
+  if (error) {
     return <p>Something went wrong: {error.message}</p>;
   }
-    
+
   const { portfolio } = res;
 
-    return (
-      <div>
-       <p>{portfolio.portfolioName} </p>
-       <p>{portfolio.userName} </p>
-       <p>{portfolio.portfolioURL} </p>
-       <p>{portfolio.contactPage.email} </p>
-      </div>
+  return (
+    <div>
+      <p>{portfolio.portfolioName} </p>
+      <p>{portfolio.userName} </p>
+      <p>{portfolio.homePage.description}</p>
+      <img src={portfolio.homePage.profilePhoto} />
+      <p>{portfolio.portfolioURL} </p>
+      <p>{portfolio.contactPage.email} </p>
+    </div>
   )
 }
