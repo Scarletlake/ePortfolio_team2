f1b8b9bfc6a451341b8f476e32c8c18bb4f041fd
Your Name <you@example.com>
2020-10-01 01:24:59 +1000

    update new filename

--- server.js
+++ server.js
@@ -22,13 +22,12 @@ app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 
 // Define Routes
 app.use('/api/user', require('./backend/routes/usersRouter'));
-app.use('/api/portfolio', require('./backend/routes/portfolioRouter'));
+app.use('/api/portfolio', require('./backend/routes/PortfolioRouter'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
-
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
