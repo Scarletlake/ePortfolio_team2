5800a58476e5c4aa3843ddf9c2b75972c1708e85
DraxZh <jielinz@student.unimelb.edu.au>
2020-10-01 20:30:12 +1000

    Update validateEmail.js

--- client/src/utils/validateEmail.js
+++ client/src/utils/validateEmail.js
@@ -1,6 +1,5 @@
 // check whether an email address is valid or not 
 export default function validateEmail (email_address) {
 
-  var re = /^\w+[\w-.]*@\w+((-\w+)|(\w*)).[a-z]{2,3}$/;
   return re.test(String(email_address));
 }
