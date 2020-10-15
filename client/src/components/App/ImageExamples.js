dd0f51263a5d3ab4ae9e5a789372da2f3d6b4605
Zhuolun WU <zhuolunw@student.unimelb.edu.au>
2020-10-15 16:11:05 +1100

    Responsively refined

---
+++ client/src/components/App/ImageExamples.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+        height: 100,
+        width: 20,
+        [theme.breakpoints.down('sm')]: {
+            width: 400,
+            height: 600,
+        },
+        [theme.breakpoints.up('sm')]: {
+            width: 580,
+            height: 700,
+        },
+        [theme.breakpoints.up('md')]: {
+            width: 880,
+            height: 700,
+        },
+        [theme.breakpoints.up('lg')]: {
+            width: 1000,
+            height: 850,
+        },
+    },
+    image: {
+        height: 100,
+        width: 20,
+        [theme.breakpoints.down('sm')]: {
+            width: 380,
+            height: 300,
+        },
+        [theme.breakpoints.up('sm')]: {
+            width: 560,
+            height: 480,
+        },
+        [theme.breakpoints.up('md')]: {
+            width: 850,
+            height: 700,
+        },
+        [theme.breakpoints.up('lg')]: {
+            width: 980,
+            height: 800,
+        },
+    }
+}));
+
+/**
+ * The example data is structured as follows:
+ *
+ * import image from 'path/to/image.jpg';
+ * [etc...]
+ *
+ * const tileData = [
+ *   {
+ *     img: image,
+ *     title: 'Image',
+ *     author: 'author',
+ *   },
+ *   {
+ *     [etc...]
+ *   },
+ * ];
+ */
+export default function ImageExamples(props) {
+    const classes = useStyles();
+    const tileData = props.tileData;
+    return (
+        <div className={classes.root}>
+            <GridList cellHeight={'auto'} className={classes.gridList} cols={1}>
+                {tileData.map((tile) => (
+                    <GridListTile key={tile.img} cols={1} className={"imgFullWidth"}>
+                        <img src={tile.img} className={classes.image} alt={tile.title} />
+                        <GridListTileBar
+                            title={tile.title}
+                            classes={{
+                                root: classes.titleBar,
+                                title: classes.title,
+                            }}
+                        />
+                    </GridListTile>
+                ))}
+            </GridList>
+        </div>
+    );
+}
--- client/src/pages/TemplatePage.js
+++ client/src/pages/TemplatePage.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Grid, Button, Typography } from '@material-ui/core';
+import { createMuiTheme, responsiveFontSizes, MuiThemeProvider } from "@material-ui/core";
+import { Card, CardActionArea, CardContent, CardMedia } from "@material-ui/core";
+import { Modal, Backdrop, Fade } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
+import ImageExamples from '../components/App/ImageExamples';
 require('../views/templatePage.css');
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    width: "60%",
+    width: "70%",
     margin: "auto",
-    marginTop: "8%",
+    marginTop: "6%",
     textAlign: "center",
   },
   grid: {
-    marginTop: "20%",
+    marginTop: "8%",
     textAlign: "center",
   },
   btn_style: {
@@ -25,67 +29,253 @@ const useStyles = makeStyles({
     marginTop: "5px",
     width: "80%",
   },
-});
+  card: {
+    maxWidth: 450,
+    margin: "auto",
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+let resFont = createMuiTheme();
+resFont = responsiveFontSizes(resFont);
+
+const tileData1 = [
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/EMG_-_SIMI.jpg',
+    title: 'dd',
+  },
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/EMG_-_SIMI.jpg',
+    title: 'dd',
+  },
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/EMG_-_SIMI.jpg',
+    title: 'dd',
+  },
+]
+
+const tileData2 = [
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/EMG_-_SIMI.jpg',
+    title: 'dd',
+  },
+]
+
+const tileData3 = [
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/EMG_-_SIMI.jpg',
+    title: 'dd',
+  },
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/EMG_-_SIMI.jpg',
+    title: 'dd',
+  },
+]
 
 export default function TemplatePage() {
+
   const classes = useStyles();
+
+  const [open1, setOpen1] = React.useState(false);
+
+  const handleClickOpen1 = () => {
+    setOpen1(true);
+  };
+
+  const handleClose1 = () => {
+    setOpen1(false);
+  };
+  const [open2, setOpen2] = React.useState(false);
+
+  const handleClickOpen2 = () => {
+    setOpen2(true);
+  };
+
+  const handleClose2 = () => {
+    setOpen2(false);
+  };
+  const [open3, setOpen3] = React.useState(false);
+
+  const handleClickOpen3 = () => {
+    setOpen3(true);
+  };
+
+  const handleClose3 = () => {
+    setOpen3(false);
+  };
+
   return (
     <div className='PageContainer'>
       < div className={classes.root}>
 
+        <MuiThemeProvider theme={resFont}>
+          <Typography variant="h2" gutterBottom>CHOOSE THE TEMPLATE YOU WANT TO USE</Typography>
+        </MuiThemeProvider>
 
-        <Typography variant="h2">CHOOSE THE TEMPLATE YOU WANT TO USE</Typography>
+        <Grid container className={classes.grid} spacing={2} >
 
-
-        <Grid container className={classes.grid}>
-
-          <Grid item xs={4}>
-            <div className="container" >
-              <img src="/envelope.png" className="image" alt="Template Page"/>
+          <Grid item xs={12} sm={12} xs={12} lg={4} xl={4} >
+            <Card className={classes.card} className="container">
+              <CardActionArea className="image" >
+                <CardMedia
+                  component="img"
+                  alt="Template Page"
+                  height='300'
+                  image="/envelope.png"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <MuiThemeProvider theme={resFont}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Business
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      The theme suitable for business uses.
+                    </Typography>
+                  </MuiThemeProvider>
+                </CardContent>
+              </CardActionArea>
               <div className="middle" >
-                <Button className={classes.btn_style} href="#">
+                <Button className={classes.btn_style} onClick={handleClickOpen1}>
                   View example
                 </Button>
                 <Button className={classes.btn_style} href="/portfolio/editor?temp=business&id=0">
                   Use this template
                 </Button>
               </div>
-            </div>
-            <h1>Business</h1>
+            </Card>
           </Grid>
 
-          <Grid item xs={4}>
-            <div className="container" >
-              <img src="/envelope.png" className="image" alt="Template Page"/>
+          <Grid item xs={12} sm={12} xs={12} lg={4} xl={4} >
+            <Card className={classes.card} className="container" >
+              <CardActionArea className="image" >
+                <CardMedia
+                  component="img"
+                  alt="Template Page"
+                  height='300'
+                  image="/envelope.png"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Minimal
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Contains basic components, your most eligent choice
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
               <div className="middle" >
-                <Button className={classes.btn_style} href="#">
+                <Button className={classes.btn_style} onClick={handleClickOpen2}>
                   View example
                 </Button>
                 <Button className={classes.btn_style} href="/portfolio/editor?temp=minimal&id=0">
                   Use this template
                 </Button>
               </div>
-            </div>
-            <h1>Minimal</h1>
+            </Card>
           </Grid>
 
-          <Grid item xs={4}>
-            <div className="container" >
-              <img src="/envelope.png" className="image" alt="Template Page"/>
+          <Grid item xs={12} sm={12} xs={12} lg={4} xl={4} >
+            <Card className={classes.card} className="container" >
+              <CardActionArea className="image" >
+                <CardMedia
+                  component="img"
+                  alt="Template Page"
+                  height='300'
+                  image="/envelope.png"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Art
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    The theme suitable for business uses.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
               <div className="middle" >
-                <Button className={classes.btn_style} href="#">
+                <Button className={classes.btn_style} onClick={handleClickOpen3}>
                   View example
                 </Button>
                 <Button className={classes.btn_style} href="/portfolio/editor?temp=art&id=0">
                   Use this template
                 </Button>
               </div>
-            </div>
-            <h1>Art</h1>
+            </Card>
           </Grid>
 
         </Grid>
-      </ div>
+      </div>
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open1}
+        onClose={handleClose1}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open1}>
+          <div className={classes.paper}>
+            <ImageExamples tileData={tileData1} />
+          </div>
+        </Fade>
+      </Modal>
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open2}
+        onClose={handleClose2}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open2}>
+          <div className={classes.paper}>
+            <ImageExamples tileData={tileData2} />
+          </div>
+        </Fade>
+      </Modal>
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open3}
+        onClose={handleClose3}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open3}>
+          <div className={classes.paper}>
+            <ImageExamples tileData={tileData3} />
+          </div>
+        </Fade>
+      </Modal>
+
     </div>
   );
 }
