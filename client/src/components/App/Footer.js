4e7480e142b5292c1aaa1867df00de09ce298011
Hui OUYNAGH <ouyangh1@student.unimelb.edu.au>
2020-10-31 22:47:12 +1100

    fix warning

--- client/src/components/App/Footer.js
+++ client/src/components/App/Footer.js
@@ -3,7 +3,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
-import {Grid} from "@material-ui/core";
 
 function Copyright() {
   return (
