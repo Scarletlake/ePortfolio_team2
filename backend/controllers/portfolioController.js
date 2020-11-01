49cf28c3bba06b0245e52d28112f6bc69d3a87be
DraxZh <jielinz@student.unimelb.edu.au>
2020-11-01 11:32:08 +1100

    Update portfolioController.js

--- backend/controllers/portfolioController.js
+++ backend/controllers/portfolioController.js
@@ -44,7 +44,6 @@ const createPortfolio = async (req, res) => {
       contactPage: contactPage
     });
 
-    portfolio.portfolioURL = "http://pandaeportfolio.herokuapp.com/portfolio/" + portfolio._id;
     portfolio.portfolioURL = "http://localhost:3000/portfolio/" + portfolio._id;
 
     // add the new portfolio to the array           
